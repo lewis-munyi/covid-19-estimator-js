1266aaa8314f0615345f9baee7dd8de909e4d1a0
lewis-munyi <lewismunyi97@gmail.com>
2020-04-11 15:02:09 +0300

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,54 @@
-const covid19ImpactEstimator = (data) => data;
+const calculateDays = (period, numberOfDays) => {
+  /*
+  * A function that receives the period type and integer count
+  * and returns the total number of days
+  * */
+
+  if (period === 'days') {
+    return numberOfDays;
+  }
+  if (period === 'weeks') {
+    return numberOfDays * 7;
+  }
+  if (period === 'months') {
+    return numberOfDays * 30;
+  }
+};
+
+const covid19ImpactEstimator = (data) => {
+  /*
+  * Challenge 1
+  * */
+
+  // Output object
+  const output = {
+    data: {}, // the input data you got
+    impact: {}, // your best case estimation
+    severeImpact: {} // your severe case estimation
+  };
+
+
+  // Derive currently infected people
+  output.impact.currentlyInfected = Math.floor(data.reportedCases * 10);
+
+  // Derive currently infected people for the severely infected
+  output.severeImpact.currentlyInfected = Math.floor(data.reportedCases * 50);
+
+  // Calculate Number of days
+  const numberOfDays = calculateDays(data.periodType, data.timeToElapse);
+
+  // Calculate Multiplication Factor
+  const factor = Math.floor(numberOfDays / 3);
+
+  // Estimate the number of infected people x days from now
+  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** factor);
+
+  // Estimate the number of severely infected people x days from now
+  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** factor);
+
+  // Return output object
+  return output;
+};
+
 
 export default covid19ImpactEstimator;
